778ac20f4150d1fade33cfe3c23828cd4a40c0bb
Sabbir1200 <hossainsabbir1200@gmail.com>
2026-06-03 22:45:36 +0600

    learned advanced Backend structuring & user Authentication

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+
+
+
+import { userRoute } from "./modules/users/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
+import { authRoute } from "./modules/auth/auth.route";
+const app: Application = express();
+
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.get("/", (req: Request, res: Response) => {
+  // res.send('Hello World!')
+  res.status(200).json({
+    message: "Express Server",
+    author: "Sabbir Hossain",
+  });
+});
+
+app.use("/api/users", userRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/auth/login",authRoute)
+
+
+
+
+
+
+
+
+
+export default app;
